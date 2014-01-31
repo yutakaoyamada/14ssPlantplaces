6fd2b4a2b3d0e5a8b4378a645b26d5cc3620b239
Yutaka <Yutaka@Leeway.wifi.uc.edu>
2014-01-31 00:25:33 -0500

    Initial Commit

---
+++ PlantPlacesMobile14SS/gen/com/plantplaces/plantplacesmobile14ss/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.plantplaces.plantplacesmobile14ss;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
